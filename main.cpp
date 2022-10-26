5a4ef23591587ab5185b290a6eeb9a34137e3c3a
LuningFang <lfang9@wisc.edu>
2022-10-26 14:47:33 -0500

    convert relative coordinate formulation in urdf to absolute ones

--- main.cpp
+++ main.cpp
@@ -38,21 +38,33 @@ using namespace chrono;
 using namespace chrono::irrlicht;
 using namespace irr;
 
+collision::ChCollisionSystemType collision_type = collision::ChCollisionSystemType::BULLET;
+
+enum class VisualizationType{
+    CYLINDER,
+    BOX,
+    MESH
+};
+
+
+
+
+
 
 // Convert a triplet (roll-pitch-yaw) to a quaternion
 ChQuaternion<> rpy2quat(const ChVector<>& rpy) {
     return Q_from_AngZ(rpy.z()) * Q_from_AngY(rpy.y()) * Q_from_AngX(rpy.x());
 }
 
-
+// link data from urdf file (relative coordinate formulation)
 struct LinkData{
     std::string name;
     ChVector<> origin_xyz;
     ChVector<> origin_rpy;
-    
+    ChVector<> dim;   // size, for dynamics and primitive visualization
 };
 
-
+// joint data from urdf file (relative coordinate formulation)
 struct JointData{
     std::string name;
     std::string link_parent;
@@ -62,40 +74,108 @@ struct JointData{
     ChVector<> axis;
 };
 
+const int numLinks = 8;
+const int numJoints = 7;
+
+LinkData scorbot_links[] = {
+    {"link0", ChVector<>(0.27, 0, 0.469), ChVector<>(0, 0, 0), ChVector<>(0.24, 0.24, 0.2274)},
+    {"link1", ChVector<>(0, 0, 0.075),    ChVector<>(0, 0, 0), ChVector<>(0.205, 0.26, 0.17)},
+    {"link2", ChVector<>(0.11, 0, 0),     ChVector<>(0, 0, 0), ChVector<>(0.28749, 0.069982, 0.169)},
+    {"link3", ChVector<>(0.12, 0, 0),     ChVector<>(0, 0, 0), ChVector<>(0.289918, 0.06, 0.15)},
+    {"link4", ChVector<>(-0.04, 0, 0),    ChVector<>(0, 0, 0), ChVector<>(0.183046, 0.073212, 0.075543)},
+    {"link5", ChVector<>(0, 0, 0.02),    ChVector<>(0, 0, 0), ChVector<>(0.06, 0.14, 0.238)},
+    {"pad1_link", ChVector<>(-0.035, 0, 0.02),    ChVector<>(0, 0, 0), ChVector<>(0.073966, 0.02, 0.01)},
+    {"pad2_link", ChVector<>(-0.035, 0, 0.02),    ChVector<>(0, 0, 0), ChVector<>(0.073966, 0.02, 0.01)}
+};
+
 JointData scorbot_joints[] = {
-    {"base_joint", "base_link", "link1", ChVector<>(0,0,0.2274), ChVector<>(0,0,0), ChVector<>(0,0,1)},
+    {"base_joint", "link0", "link1", ChVector<>(0,0,0.2274), ChVector<>(0,0,0), ChVector<>(0,0,1)},
     {"shoulder_joint", "link1", "link2", ChVector<>(0.0255, 0, 0.13), ChVector<>(-CH_C_PI_2, 0, -CH_C_PI_2), ChVector<>(0,0,1)},
-    {"elbow_joint", "link2", "link3", ChVector<>(0.2225, 0, 0), ChVector<>(0,0,0), ChVector<>(0,0,1)}
+    {"elbow_joint", "link2", "link3", ChVector<>(0.2225, 0, 0), ChVector<>(0,0,0), ChVector<>(0,0,1)},
+    {"pitch_joint", "link3", "link4", ChVector<>(0.2225, 0, 0), ChVector<>(1.570796327,0,0), ChVector<>(0,1,0)},
+    {"roll_joint", "link4", "link5", ChVector<>(0,0,0), ChVector<>(0, -1.570796327, 0), ChVector<>(0,0,1)},
+    {"pad1_joint", "link5", "pad1_link", ChVector<>(0, -0.0545, -0.0905), ChVector<>(CH_C_PI_2, -0.74758, -CH_C_PI_2), ChVector<>(0, 0, 1) },
+    {"pad2_joint", "link5", "pad2_link", ChVector<>(0, 0.0545, -0.0905), ChVector<>(-CH_C_PI_2, -0.74758, CH_C_PI_2), ChVector<>(0, 0, -1) }
 };
 
 
+// given link name, return LinkData, note that this is for child only, parent is based on previous result!
+LinkData LookupLinkData(std::string linkname){
+    LinkData tmp_link;
+    for (int i = 0; i < numLinks; i++){
+        tmp_link = scorbot_links[i];
+        if (tmp_link.name.compare(linkname) == 0){
+            break;
+        }
+    }
+    return tmp_link;
+}
 
-collision::ChCollisionSystemType collision_type = collision::ChCollisionSystemType::BULLET;
+// add visual assets to the body
+void AddVisualizationShape(std::shared_ptr<ChBodyAuxRef> body_ptr,
+                           VisualizationType type){
+    if (type == VisualizationType::BOX){
+        auto box_shape = chrono_types::make_shared<ChBoxShape>();
+        box_shape->GetBoxGeometry().SetLengths(LookupLinkData(body_ptr->GetNameString()).dim);
+        box_shape->SetColor(ChColor(ChRandom(), ChRandom(), ChRandom()));
+        body_ptr->AddVisualShape(box_shape);
+
+        // std::cout << "add shape of dimenstion" << LookupLinkData(body_ptr->GetNameString()).dim * 0.2 << " at location " <<  body_ptr->GetFrame_COG_to_abs() << std::endl;
+    }
+
+    if (type == VisualizationType::MESH){
+        std::string vis_mesh_file = "/home/luning/CollaborateProjects/scorbot/data/obj/base_Link.obj";
+        auto trimesh_vis = geometry::ChTriangleMeshConnected::CreateFromWavefrontFile(vis_mesh_file, true, true);
+
+        auto trimesh_shape = chrono_types::make_shared<ChTriangleMeshShape>();
+        trimesh_shape->SetMesh(trimesh_vis);
+        trimesh_shape->SetName("base_Link");
+        trimesh_shape->SetMutable(false);
+
+        trimesh_shape->SetColor(ChColor(0.6f, 0, 0));
+
+        body_ptr->AddVisualShape(trimesh_shape);
+    }
+
+    if (type == VisualizationType::CYLINDER){
+        auto cyl2 = chrono_types::make_shared<ChCylinderShape>();
+        cyl2->GetCylinderGeometry().p1 = ChVector<>(0, 0, 0);
+        cyl2->GetCylinderGeometry().p2 = ChVector<>(0.24, 0, 0);
+        cyl2->GetCylinderGeometry().rad = 0.03;
+        cyl2->SetColor(ChColor(0.2, 0.8, 0.3f));
+        body_ptr->AddVisualShape(cyl2);
+    }
+}
 
 
-// auxillary frame, not at the center of gravity!!
-std::shared_ptr<ChBodyAuxRef> createChildAuxRef(ChQuaternion<> parent_frame,
-                                           ChVector<> parent_location,
-                                           int i // joint id (start from 0)
-                                           ){
+// auxillary frame, not at the center of gravity
+// note that the parent info is based on its parents, 
+std::shared_ptr<ChBodyAuxRef> createChildAuxRef(ChQuaternion<> parent_quat,
+                                                ChVector<> parent_location,
+                                                int i // joint id (start from 0)
+                                                ){
     auto child = chrono_types::make_shared<ChBodyAuxRef>();
-    ChMatrix33<> parentRot(parent_frame);
-    ChMatrix33<> jointRot(rpy2quat(scorbot_joints[i].origin_rpy)); // this need to be changed ...
+    ChQuaternion<> joint_quat = rpy2quat(scorbot_joints[i].origin_rpy); // this need to be changed ...
 
-    ChVector<double> joint_location_global = parent_location + parentRot * scorbot_joints[i].origin_xyz;
-    
+    // perform transformation using quaternion to get global location of the joint
+    ChVector<double> joint_location_global = parent_location + parent_quat.Rotate(scorbot_joints[i].origin_xyz);
 
-    child->SetFrame_REF_to_abs(ChFrame<>(joint_location_global, rpy2quat(scorbot_joints[i].origin_rpy)));
-  
-    child->SetFrame_COG_to_REF(ChFrame<>(ChVector<>(0.11, 0, 0), ChQuaternion<>(1, 0, 0, 0)));
+    // std::cout << "parent location: aux wrt global " << parent_location << std::endl;
 
+    child->SetFrame_REF_to_abs(ChFrame<>(joint_location_global, joint_quat));  
+    // child info can be read directly from urdf! 
+    LinkData child_data = LookupLinkData(scorbot_joints[i].link_child);
+    child->SetFrame_COG_to_REF(ChFrame<>(child_data.origin_xyz, rpy2quat(child_data.origin_rpy)));
     return child;
 } 
+
 
 
 int main(int argc, char* argv[]) {
     GetLog() << "Copyright (c) 2017 projectchrono.org\nChrono version: " << CHRONO_VERSION << "\n\n";
     ChSystemNSC sys;
+    std::unordered_map<std::string,  std::shared_ptr<ChBodyAuxRef>> name_link_map; // map that store link and the pointer to the body once created in Chrono (in absolute coordinate frames!)
+
 
     // start with prototype
     bool useMesh = false;
@@ -112,139 +192,62 @@ int main(int argc, char* argv[]) {
     ChVector<double> dim_link1(0.205, 0.26, 0.17);
     ChVector<double> dim_link2(0.28749, 0.069982, 0.169);
 
-    // ChVector<double> dim_base_link(0.024, 0.024, 0.02274);
-    // ChVector<double> dim_link1(0.0205, 0.026, 0.017);
-    // ChVector<double> dim_link2(0.028749, 0.0069982, 0.0169);
-
-
-    // ChVector<double> pos_base_link(0.27, 0.0, 0.469);
-    ChVector<double> pos_base_link(0.27, 0.0, 0.469);
-    ChVector<double> pos_base_joint = pos_base_link + ChVector<double>(0, 0, 0.2274);
-    ChVector<double> pos_link1 = pos_base_joint + ChVector<double>(0, 0, 0.075);
-
-
-
-
-    // ChVector<double> pos_link1(0.27, 0.0, 0.469);
 
     auto link_mat = chrono_types::make_shared<ChMaterialSurfaceNSC>();
     auto link_mat_vis = chrono_types::make_shared<ChVisualMaterial>(*ChVisualMaterial::Default());
 
     std::shared_ptr<collision::ChCollisionModel> collision_model = chrono_types::make_shared<collision::ChCollisionModelBullet>();
-    auto base_link = chrono_types::make_shared<ChBodyEasyBox>(dim_base_link.x(), 
-                                                              dim_base_link.y(), 
-                                                              dim_base_link.z(),  // x,y,z size
-                                                              100,        // density
-                                                              true,       // visualization?
-                                                              true,
-                                                              link_mat,
-                                                              collision_model);     // collision?
-    base_link->SetPos(pos_base_link);
+
+    // add base link, start from origin, add name to aux ref for easy lookup?
+    auto base_link = chrono_types::make_shared<ChBodyAuxRef>();
+    LinkData base_link_data = scorbot_links[0];
+    base_link->SetNameString(base_link_data.name);
+    base_link->SetFrame_REF_to_abs(ChFrame<>(base_link_data.origin_xyz, rpy2quat(base_link_data.origin_rpy)));
+    base_link->SetFrame_COG_to_REF(ChFrame<>(ChVector<>(0, 0, 0.1137), ChQuaternion<>(1,0,0,0)));  
+
     base_link->SetBodyFixed(true);
+    base_link->SetCollide(false);
     base_link->SetMass(mass_base);
-
+    base_link->SetInertiaXX(ChVector<>(0.2, 0.2, 0.2)); // TODO: inertia? w.r.t. which axes?
     
-    if (useMesh) {
-        std::string vis_mesh_file = "/home/luning/CollaborateProjects/scorbot/data/obj/base_Link.obj";
-        auto trimesh_vis = geometry::ChTriangleMeshConnected::CreateFromWavefrontFile(vis_mesh_file, true, true);
-            // trimesh_vis->Transform(m_mesh_xform.GetPos(), m_mesh_xform.GetA());  // translate/rotate/scale mesh
-            // trimesh_vis->RepairDuplicateVertexes(1e-9);                          // if meshes are not watertight
+    AddVisualizationShape(base_link, VisualizationType::BOX);
+    sys.Add(base_link);
+    std::cout << "link 0 absolute position of COG: " << base_link->GetFrame_COG_to_abs() << std::endl;
+    // insert this to my map
+    name_link_map[base_link_data.name] = base_link;
 
-        auto trimesh_shape = chrono_types::make_shared<ChTriangleMeshShape>();
-        trimesh_shape->SetMesh(trimesh_vis);
-        trimesh_shape->SetName("base_Link");
-        trimesh_shape->SetMutable(false);
-        // ChFrame<> transform(pos_base_link, QUNIT);    
-        // ChFrame<> transform(ChVector<double>(0, 0, 0), QUNIT);    
-
-        // trimesh_vis->Transform(transform.GetPos(), transform.GetA());  // translate/rotate/scale mesh
-        trimesh_shape->SetColor(ChColor(0.6f, 0, 0));
-
-        base_link->AddVisualShape(trimesh_shape);
-    }
-
-    sys.AddBody(base_link);
+    for (int i = 0; i < numJoints; i++){
+        // find body pointer 
+        std::string parent_name = scorbot_joints[i].link_parent;
+        std::shared_ptr<ChBodyAuxRef> parent_body = name_link_map.find(parent_name)->second; // parent body
 
 
+        std::shared_ptr<ChBodyAuxRef> child_body  = createChildAuxRef(parent_body->GetFrame_REF_to_abs().GetRot(), parent_body->GetFrame_REF_to_abs().GetPos(), i);
+        child_body->SetNameString(scorbot_joints[i].link_child);
 
-    std::shared_ptr<collision::ChCollisionModel> collision_model2 = chrono_types::make_shared<collision::ChCollisionModelBullet>();
-    auto link1 = chrono_types::make_shared<ChBodyEasyBox>(dim_link1.x(), 
-                                                              dim_link1.y(), 
-                                                              dim_link1.z(),  // x,y,z size
-                                                              100,        // density
-                                                              true,       // visualization?
-                                                              true,
-                                                              link_mat,
-                                                              collision_model2);     // collision?
-    link1->SetPos(pos_link1);
-    link1->SetBodyFixed(true);
-    link1->SetMass(mass_link1);
+        // std::cout << "child name: " << child_body->GetNameString() << " absolute position of COG: " << child_body->GetFrame_COG_to_abs() << std::endl;
+        child_body->SetBodyFixed(true);
+        child_body->SetMass(mass_link1);  // TODO mass
+        child_body->SetCollide(false);
+        child_body->SetInertiaXX(ChVector<>(0.2, 0.2, 0.2)); // TODO: inertia? w.r.t. which axes?
+    
+        AddVisualizationShape(child_body, VisualizationType::BOX);
+        sys.Add(child_body);
+        
+        // insert this to my map
+        name_link_map[child_body->GetNameString()] = child_body;
 
-    if (useMesh){
-        std::string shoulder_mesh_file = "/home/luning/CollaborateProjects/scorbot/data/obj/shoulder_Link.obj";
-        auto shoulder_vis = geometry::ChTriangleMeshConnected::CreateFromWavefrontFile(shoulder_mesh_file, true, true);
-            // trimesh_vis->Transform(m_mesh_xform.GetPos(), m_mesh_xform.GetA());  // translate/rotate/scale mesh
-            // trimesh_vis->RepairDuplicateVertexes(1e-9);                          // if meshes are not watertight
-
-        auto shoulder_trimesh_shape = chrono_types::make_shared<ChTriangleMeshShape>();
-        shoulder_trimesh_shape->SetMesh(shoulder_vis);
-        shoulder_trimesh_shape->SetName("shoulder_Link");
-        shoulder_trimesh_shape->SetMutable(false);
-        // trimesh_vis->Transform(transform_link1.GetPos(), transform_link1.GetA());  // translate/rotate/scale mesh
-        base_link->AddVisualShape(shoulder_trimesh_shape);
 
     }
-
-
-
-    sys.AddBody(link1);
-
-    // link2
-    ChVector<> shoulder_joint_eu(-CH_C_PI_2, -CH_C_PI_2, 0);
-    ChQuaternion<> shoulder_joint_frame = Q_from_Euler123(shoulder_joint_eu);
-    std::shared_ptr<ChBodyAuxRef> link2 = createChildAuxRef(link1->GetRot(), link1->GetPos(), 1);
-
-    link2->SetBodyFixed(true);
-    link2->SetCollide(false);
-    link2->SetMass(mass_link2);
-    link2->SetInertiaXX(ChVector<>(0.2, 0.2, 0.2)); // inertia? w.r.t. which axes?
-    
-    auto cyl = chrono_types::make_shared<ChCylinderShape>();
-    cyl->GetCylinderGeometry().p1 = ChVector<>(0, 0, 0);
-    cyl->GetCylinderGeometry().p2 = ChVector<>(0.22, 0, 0);
-    cyl->GetCylinderGeometry().rad = 0.05;
-    cyl->SetColor(ChColor(0, 0, 0.6f));
-    link2->AddVisualShape(cyl);
-    
-    sys.Add(link2);
-    std::cout << "after adding body\n: body frame: \n" << link2->GetRot() << std::endl;
-
-    // link3 -- pitch
-    ChVector<> elbow_joint_eu(0,0,0);
-    ChQuaternion<> elbow_joint_frame = Q_from_Euler123(elbow_joint_eu);
-    ChVector<> elbow_joint_location(0.2225, 0, 0);
-    std::shared_ptr<ChBodyAuxRef> link3 = createChildAuxRef(link2->GetFrame_REF_to_abs().GetRot(), link2->GetFrame_REF_to_abs().GetPos(), 2);
-
-
-    auto cyl2 = chrono_types::make_shared<ChCylinderShape>();
-    cyl2->GetCylinderGeometry().p1 = ChVector<>(0, 0, 0);
-    cyl2->GetCylinderGeometry().p2 = ChVector<>(0.24, 0, 0);
-    cyl2->GetCylinderGeometry().rad = 0.03;
-    cyl2->SetColor(ChColor(0.2, 0.8, 0.3f));
-    link3->AddVisualShape(cyl2);
-    link3->SetBodyFixed(true);
-    sys.Add(link3);
-
-
 
     // revolute joint motor between base and shoulder
-    auto rev_base_shoulder = chrono_types::make_shared<ChLinkMotorRotationSpeed>();
-    rev_base_shoulder->Initialize(base_link, link1, ChFrame<>(pos_base_joint, ChQuaternion<>(1, 0, 0, 0)));
-    auto mwspeed =
-        chrono_types::make_shared<ChFunction_Const>(CH_C_PI_4);  // constant angular speed, in [rad/s], 1PI/s =180°/s
+    // auto rev_base_shoulder = chrono_types::make_shared<ChLinkMotorRotationSpeed>();
+    // rev_base_shoulder->Initialize(base_link, link1, ChFrame<>(pos_base_joint, ChQuaternion<>(1, 0, 0, 0)));
+    // auto mwspeed =
+    //     chrono_types::make_shared<ChFunction_Const>(CH_C_PI_4);  // constant angular speed, in [rad/s], 1PI/s =180°/s
 
-    rev_base_shoulder->SetSpeedFunction(mwspeed);
-    sys.AddLink(rev_base_shoulder);
+    // rev_base_shoulder->SetSpeedFunction(mwspeed);
+    // sys.AddLink(rev_base_shoulder);
 
     // Add limits to the Z rotation of the revolute joint
     // double min_angle = 0;
